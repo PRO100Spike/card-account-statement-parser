f7c56bb3b634d5cb605eaaf0cc4f165c6688057b
procyon Spike <13spike@.mail.ru>
2025-10-04 10:07:17 +0300

    refactor: replace code on file

--- __main__.py
+++ __main__.py
@@ -7,10 +7,12 @@ from dotenv import load_dotenv
 
 # --- КОНФИГУРАЦИЯ ---
 load_dotenv()
+
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
+
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"
 
 FILE_PATH = 'CardAccountStatement.csv'
